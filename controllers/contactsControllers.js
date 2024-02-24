170e8588b5e8d1f3a8c7ac142c661d65444ba468
VuktorTS <136127912+VuktorTS@users.noreply.github.com>
2024-02-25 00:25:22 +0200

    added controllers getAllContacts and getOneContact

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,11 +1,30 @@
-import contactsService from "../services/contactsServices.js";
+import HttpError from "../helpers/HttpError.js";
+import { listContacts, getContactById } from "../services/contactsServices.js";
 
-export const getAllContacts = (req, res) => {};
+export const getAllContacts = async (req, res, next) => {
+  try {
+    const result = await listContacts();
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const getOneContact = (req, res) => {};
+export const getOneContact = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await getContactById(id);
+    if (!result) {
+      throw HttpError(404);
+    }
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const deleteContact = (req, res) => {};
+export const deleteContact = async (req, res) => {};
 
-export const createContact = (req, res) => {};
+export const createContact = async (req, res) => {};
 
-export const updateContact = (req, res) => {};
+export const updateContact = async (req, res) => {};
