5575a355e3eaf4b6b1c585bc14d4a1d31ea6d9db
VuktorTS <136127912+VuktorTS@users.noreply.github.com>
2024-02-25 02:11:30 +0200

    adde controllers updateContact

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,9 +1,13 @@
 import HttpError from "../helpers/HttpError.js";
-import { createContactSchema } from "../schemas/contactsSchemas.js";
+import {
+  createContactSchema,
+  updateContactSchema,
+} from "../schemas/contactsSchemas.js";
 import {
   listContacts,
   getContactById,
   addContact,
+  updateContactById,
 } from "../services/contactsServices.js";
 
 export const getAllContacts = async (req, res, next) => {
@@ -41,6 +45,22 @@ export const createContact = async (req, res, next) => {
   }
 };
 
-export const updateContact = async (req, res) => {};
+export const updateContact = async (req, res, next) => {
+  try {
+    const { error } = updateContactSchema.validate(req.body);
+    console.log(updateContactSchema.validate(req.body));
+    if (error) {
+      throw HttpError(400);
+    }
+    const { id } = req.params;
+    const result = await updateContactById(id, req.body);
+    if (!result) {
+      throw HttpError(404);
+    }
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
 
 export const deleteContact = async (req, res) => {};
