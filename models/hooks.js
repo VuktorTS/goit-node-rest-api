e4618573fa56698d02dfafd69bc0f740918e32f2
VuktorTS <136127912+VuktorTS@users.noreply.github.com>
2024-03-07 19:20:30 +0200

    fix bug

--- models/hooks.js
+++ models/hooks.js
@@ -3,7 +3,7 @@ export const handleSaveError = (error, data, next) => {
   next();
 };
 export const setUpdateSettings = function (next) {
-  this.option.new = true;
-  this.option.runValidators = true;
+  this.options.new = true;
+  this.options.runValidators = true;
   next();
 };
--- services/contactsServices.js
+++ services/contactsServices.js
@@ -1,4 +1,4 @@
-import Contact from "../models/Contact.js";
+import Contact from "../models/contact.js";
 
 export const getAllContacts = () => Contact.find();
 
