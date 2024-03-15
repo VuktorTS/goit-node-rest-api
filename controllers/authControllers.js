7d8a762438ff5ac67fe715d85af2b1775e8188bb
VuktorTS <136127912+VuktorTS@users.noreply.github.com>
2024-03-15 15:12:33 +0200

    add signup controller and services

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,9 +1,19 @@
 import * as authServices from "../services/authServices.js";
 import { controllerWraper } from "../helpers/controllerWraper.js";
+import HttpError from "../helpers/HttpError.js";
+
 const signup = async (req, res) => {
+  const { email } = req.body;
+  const user = await authServices.findUser({ email });
+
+  if (user) {
+    throw HttpError(409, "Email in use");
+  }
+
   const newUser = await authServices.signup(req.body);
   res.status(201).json({ username: newUser.username, email: newUser.email });
 };
+
 export default {
   signup: controllerWraper(signup),
 };
