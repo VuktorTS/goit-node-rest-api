0b825f037885d326347d694ad6c55c566122434e
VuktorTS <136127912+VuktorTS@users.noreply.github.com>
2024-03-15 15:46:22 +0200

    add hash password

--- services/authServices.js
+++ services/authServices.js
@@ -1,5 +1,9 @@
 import User from "../models/User.js";
+import bcrypt from "bcrypt";
 
 export const findUser = (filter) => User.findOne(filter);
 
-export const signup = (date) => User.create(date);
+export const signup = async (date) => {
+  const hashPassword = await bcrypt.hash(date.password, 10);
+  return User.create({ ...date, password: hashPassword });
+};
